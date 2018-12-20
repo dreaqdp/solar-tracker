44feae1a4fc13b63b40f55946807e1f4f98c7644
Andrea Querol De Porras <andrea.querol@c5s202pc104.fib.upc.es>
2018-12-20 09:36:33 +0100

    Project ended. Bye world

--- controller/controller.ino
+++ controller/controller.ino
@@ -3,11 +3,16 @@
 #define STANDBY 0
 #define CONTROL 1
 
+#define SERVO_STEP 2
+#define LDR_STEP 5
+#define THRESH 825
+
 const int LED = 4;
 const int button = 2; // interrupt pin
 
 int old_val = 0;
 volatile int state = STANDBY;
+int init_control = 0;
 volatile int button_state = LOW;
 int led_state = 1;
 
@@ -38,8 +43,6 @@ void setup() {
   
   servox.attach(9);
   servoy.attach(10);
-
-  old_top = old_bottom = old_left = old_right = 700;
   
   attachInterrupt(digitalPinToInterrupt(button), button_interrupt, CHANGE);
 }
@@ -47,15 +50,16 @@ void setup() {
 void button_interrupt () {
   int but = digitalRead(button);
   if (but == HIGH && button_state == LOW) {
-    Serial.println("HIGH");
+    //Serial.println("HIGH");
     button_state = HIGH;
     start_time = millis();
   } else if (but == LOW && button_state == HIGH) {
-    Serial.println("LOW\n----");
+    //Serial.println("LOW\n----");
     button_state = LOW;
     int delta = millis() - start_time;
     if (delta >= 1500) {
       state = (state == STANDBY ? CONTROL : STANDBY);
+      if(state) init_control = 1;
     }
   }
 }
@@ -74,15 +78,23 @@ void ldr_stats (int val_ldr) {
 }
 
 void loop() {
-  //Serial.println(digitalRead(button));
   bt_state = ' ';
   if (state == STANDBY) {
+    led_state = 0;
     digitalWrite(LED, HIGH);
     posx = 180;
+    servox.write(posx);
     posy = 90;
+    servoy.write(posy);
+    old_top = old_bottom = old_left = old_right = THRESH;
   }
   else {
-    posx = 90;
+    if (init_control) {
+      posx = 90;
+      init_control = 0;
+    }
+
+    // Control LED
     if (!led_state) {
       digitalWrite(LED, HIGH);
       delay(100);
@@ -92,53 +104,59 @@ void loop() {
       digitalWrite(LED, LOW);
       delay(100);
       led_state = 0;
+    }
     
-      // ldr
-      val_tl = analogRead(ldr_tl);
-      vout_tl = val_tl * vfactor;
-      //Serial.println(val_tl);
-      val_tr = analogRead(ldr_tr);
-      vout_tr = val_tr * vfactor;
-      //Serial.println(val_tr);
-      val_bl = analogRead(ldr_bl);
-      vout_bl = val_bl * vfactor;
-      //Serial.println(val_bl);
-      val_br = analogRead(ldr_br);
-      vout_br = val_br * vfactor;
-      //Serial.println(val_br);
-      top = (val_tl + val_tr)/2;
-      bottom = (val_bl + val_br)/2;
-      left  = (val_tl + val_bl)/2;
-      right = (val_tr + val_br)/2;
+    // Llegim LDR
+    val_tl = analogRead(ldr_tl);
+    vout_tl = val_tl * vfactor;
+    val_tr = analogRead(ldr_tr);
+    vout_tr = val_tr * vfactor;
+    val_bl = analogRead(ldr_bl);
+    vout_bl = val_bl * vfactor;
+    val_br = analogRead(ldr_br);
+    vout_br = val_br * vfactor;
 
-      // servos
-      if (top > 700 || bottom > 700 || left > 700 || right > 700) {
-        if (top > bottom && posx > 0 && top >= old_top+5) {
-          posx -= 5;
-        }
-        else if (top < bottom && posx < 180 && bottom >= old_bottom+5) {
-          posx += 5;
-        }
-        old_bottom = bottom;
-        old_top = top;
-      
-        if (left > right && posy < 180 && left > old_left+5) {
-          posy += 5;
-        }
-        else if (left < right && posy > 0 && right > old_right+5) {
-          posy -= 5;
-        }
-        old_left = left;
-        old_right = right;
+    // mitjanes
+    top = (val_tl + val_tr)/2;
+    bottom = (val_bl + val_br)/2;
+    left  = (val_tl + val_bl)/2;
+    right = (val_tr + val_br)/2;
+
+    // servos
+    if (top > THRESH || bottom > THRESH || left > THRESH || right > THRESH) {
+      // top - bottom
+      if (top >= bottom+LDR_STEP && posx > 0 && top >= old_top + LDR_STEP) {
+        posx -= SERVO_STEP;
+        old_top += LDR_STEP;
+        old_bottom = THRESH;
       }
-      else old_top = old_bottom = old_left = old_right = 700;
+      else if (top+LDR_STEP <= bottom && posx < 180 && bottom >= old_bottom + LDR_STEP) {
+        posx += SERVO_STEP;
+        old_bottom += LDR_STEP;
+        old_top = THRESH;
+      }
+
+      // left-right
+      if (left >= right+LDR_STEP && posy < 180 && left >= old_left+LDR_STEP) {
+        posy += SERVO_STEP;
+        old_left += LDR_STEP;
+        old_right = THRESH;
+      }
+      else if (left+LDR_STEP <= right && posy > 0 && right >= old_right+LDR_STEP) {
+        posy -= SERVO_STEP;
+        old_right += LDR_STEP;
+        old_left = THRESH;
+      }     
+    }
+    else { // Low luminosity, sleep!
+      old_top = old_bottom = old_left = old_right = THRESH;
+      state = STANDBY;
     }
   }
 
-
   // Movem els motors
   if (posx != oldx) {
-    //Serial.println(posx);
+    Serial.println(posx);
     servox.write(posx);
     oldx = posx;
   }
@@ -157,6 +175,7 @@ void loop() {
     }
     else if (bt_state == 'y') { // suposat boto de passar a activar stma
       state = CONTROL;
+      init_control = 1;
     }
     else if (bt_state == 'a') { // boto petició estat stma
       BTSerial.println(state);
@@ -175,9 +194,7 @@ void loop() {
       BTSerial.println(vout_tl);
       BTSerial.println(vout_tr);
       BTSerial.println(vout_bl);
-      BTSerial.println(vout_br);
-      
-      
+      BTSerial.println(vout_br);      
     }
     else if (bt_state == 'e') { // boto peticio ldr resistencies
       float res_tl = (5-vout_tl)*10000/vout_tl; // RL = (Vin - Vout)Rs/Vout
@@ -189,9 +206,8 @@ void loop() {
       BTSerial.println(res_bl);
       BTSerial.println(res_br);      
     }
+    BTSerial.println();
   }
-
-  Serial.println("--------------");
   
-  delay (500);
+  delay (100);
 }
